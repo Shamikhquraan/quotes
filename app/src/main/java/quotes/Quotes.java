7bcde71607744237629558cf4118cce6b7cbdf75
shamikhquraan1 <shamikhq@yahoo.com>
2021-10-25 03:30:20 +0300

    adding lab08

--- app/src/main/java/quotes/Quotes.java
+++ app/src/main/java/quotes/Quotes.java
@@ -1,10 +1,9 @@
 package quotes;
-
-
 public class Quotes {
     private String author;
     private String text;
-
+//that what im said like tamplte :
+    //there is a get and set for author ,
     public String getAuthor() {
         return author;
     }
@@ -20,12 +19,12 @@ public class Quotes {
     public void setText(String text) {
         this.text = text;
     }
-
     @Override
     public String toString() {
-        String msg ="Today's Quote: " +
+        //here the qoute after u get the text :
+        String qoute ="Today's Quote: " +
                 "Quote: " + text +
                 "author: " + author ;
-        return msg ;
+        return qoute ;
     }
 }
